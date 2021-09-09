0da9f06af9ec7253caa770ae49d6c61105518a6d
SAKALLI <89208116+burkay2323@users.noreply.github.com>
2021-09-09 17:53:15 +0200

    Update Test_Class.java

--- src/Test_Class.java
+++ src/Test_Class.java
@@ -2,5 +2,6 @@ public class Test_Class {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
         System.out.println("Github dan merhaba");
+        System.out.println("Github dan tekrar merhaba");
     }
 }
